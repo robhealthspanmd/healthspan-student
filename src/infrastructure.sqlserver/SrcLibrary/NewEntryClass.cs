164a36e630531c9e3332828a9a853a009f4dad99
lsgoad <lsgoad@asu.edu>
2023-04-19 11:17:37 -0700

    updated NewEntryClass
    added connection string and a get method to return all entries

--- src/infrastructure.sqlserver/SrcLibrary/NewEntryClass.cs
+++ src/infrastructure.sqlserver/SrcLibrary/NewEntryClass.cs
@@ -3,14 +3,16 @@ using System.Data.SqlClient;
 
 public class NewEntryClass
 {
-    private readonly string _connectionString;
+    private readonly string _connectionString = "Server=healthspanmd.database.windows.net;Database=HealthspanMDMarketing;user id=marketing; password=h3992-MK.2993pzjj7;";
 
-    public NewEntryClass(string connectionString)
+    struct entry
     {
-        _connectionString = connectionString;
+        public string name;
+        public string email;
+        public int healthAge;
     }
 
-    public void InsertNewEntry(string name, string email, int healthAge)
+    public void insertNewEntry(string name, string email, int healthAge)
     {
         using (SqlConnection connection = new SqlConnection(_connectionString))
         {
@@ -28,4 +30,31 @@ public class NewEntryClass
             }
         }
     }
+
+    public entry[] getEntries()
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+        List<entry> entries = new List<entry>();
+            connection.Open();
+             using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        entry e = new entry();
+                        e.name = reader.GetString(0);
+                        e.email = reader.GetString(1);
+                        e.healthAge = reader.GetInt32(2);
+                        entries.Add(e);
+                    }
+                }
+            }
+            connection.Close();
+        
+
+        // Convert the list to an array and return it.
+        return entries.ToArray();
+        }
+
+
 }
